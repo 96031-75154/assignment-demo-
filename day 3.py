252edccabe192fe7f6b56dd2f0007c454b5bf617
96031-75154 <89204724+96031-75154@users.noreply.github.com>
2021-08-24 17:45:55 +0530

    Day 3 assignment

---
+++ day 3.py
@@ -0,0 +1,18 @@
+Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=2
+>>> b=4
+>>> c=a%b
+>>> print(c)
+2
+>>> a=3
+>>> b=6
+>>> c=a%b
+>>> print(c)
+3
+>>> a=5
+>>> b=10
+>>> c=a%b
+>>> print(c)
+5
+>>> 
